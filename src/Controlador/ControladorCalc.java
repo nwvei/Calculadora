c6105cd390e1334bad9aea8a7abb5d4e82111e9e
nwvei <aaronvillegasl04@gmail.com>
2026-08-03 15:29:42 -0600

    ControladorCalculadora
    Se realizo el controlador con su funcion de calcular y su funcion de reiniciar o ac

---
+++ src/Controlador/ControladorCalc.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import VistaCalculadora.FrmCalculadora;
+import java.util.Set;
+import modelo.calculadora;
+import modelo.InvalidValueException;
+import modelo.DivisionZeroException;
+
+/**
+ *
+ * @author Aaron
+ */
+public class ControladorCalc {
+    private double value;
+    private double resultado;
+    private String operacion;
+     private FrmCalculadora vista;
+    private calculadora modelo;
+
+    public ControladorCalc(FrmCalculadora vista) {
+        this.vista = vista;
+        this.modelo = new calculadora();
+    }
+    public void calcular(double valor,String operador) throws InvalidValueException, DivisionZeroException{
+        modelo.setValor(valor);
+        switch(modelo.getOperacion()){
+            case "+" -> {
+                modelo.sumar();
+                
+            }
+            case"-" -> {
+                modelo.restar();
+            }
+            case "*" -> {
+                modelo.multiplicar();
+            }
+            case "/" -> {
+                modelo.dividir();
+            }
+            default -> {
+                modelo.setOperacion(operador);
+                vista.mostarResultado.ValueOf(modelo.getResultado());
+                        }
+}
+    }
+    public void acPresionado() {
+    modelo.reiniciar();
+    vista.setTextoPantalla("0");
+}
+}
